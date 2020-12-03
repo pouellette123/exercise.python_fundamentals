170c0900e27470c937113566ab0a2c26fe4adc35
Paul Ouellette <pouellette123es@gmail.com>
2020-12-03 13:18:23 -0500

    updated source - removed tabs

--- src/main/predicator.py
+++ src/main/predicator.py
@@ -1,29 +1,25 @@
 # Created by Leon Hunter at 3:56 PM 10/23/2020
 class Predicator(object):
     def is_greater_than_5(self, some_value):
-	if some_value > 5:
-		return True
-	else:
-		return False
-        #return None # TODO - Implement solution
+        if some_value > 5:
+            return True
+        else:
+            return False
 
     def is_greater_than_8(self, some_value):
-	if some_value > 8:
-		return True
-	else:
-		return False
-        # return None # TODO - Implement solution
+        if some_value > 8:
+            return True
+        else:
+            return False
 
     def is_less_than_4(self, some_value):
-	if some_value < 4:
-		return True
-	else:
-		return False
-        #return None # TODO - Implement solution
+        if some_value < 4:
+            return True
+        else:
+            return False
 
     def is_less_than_1(self, some_value):
-	if some_value < 1:
-		return True
-	else:
-		return False
-        #return None # TODO - Implement solution
+        if some_value < 1:
+            return True
+        else:
+            return False
--- src/main/string_evaluator.py
+++ src/main/string_evaluator.py
@@ -1,44 +1,37 @@
 # Created by Leon Hunter at 3:57 PM 10/23/2020
 class StringManipulator(object):
     def get_hello_world(self):
-		return "Hello World"
-        #return None # TODO - Implement solution
+        return 'Hello World'
 
     def concatenate(self, value_to_be_added_to, value_to_add):
-		return value_to_be_added_to + value_to_add
-        #return None # TODO - Implement solution
+        return value_to_be_added_to + value_to_add
 
     def substring_inclusive(self, string_to_fetch_from, starting_index, ending_index):
-		return string_to_fetch_from(starting_index:ending_index+1)
-        #return None # TODO - Implement solution
+        return string_to_fetch_from[starting_index:ending_index+1]
 
     def substring_exclusive(self, string_to_fetch_from, starting_index, ending_index):
-		return string_to_fetch_from(starting_index+1, ending_index-1)
-        #return None # TODO - Implement solution
+        return string_to_fetch_from[starting_index+1, ending_index-1]
 
     def compare(self, first_value, second_value):
-		if first_value == second_value:
-			return True
-		else:
-			return False
-        #return None # TODO - Implement solution
+        if first_value == second_value:
+            return True
+        else:
+            return False
 
     def get_middle_character(self, string_to_fetch_from):
-		len_string = len(string_to_fetch_from)
-		middle=len_string/2
-		return string_to_fetch_from(middle:middle+1)
-        #return None # TODO - Implement solution
+        len_string = len(string_to_fetch_from)
+        middle=len_string/2
+        return string_to_fetch_from[middle:middle+1]
 
     def get_first_word(self, string_to_fetch_from):
-		tmp_list=string_to_fetch_from.split()
-		return tmp_list[0]
-        #return None # TODO - Implement solution
+        tmp_list=string_to_fetch_from.split()
+        return tmp_list[0]
 
     def get_second_word(self, string_to_fetch_from):
-		tmp_list=string_to_fetch_from.split()
-		return tmp_list[1]
-        #return None # TODO - Implement solution
+        tmp_list=string_to_fetch_from.split()
+        return tmp_list[1]
 
     def reverse(self, string_to_be_reversed):
-		return string_to_be_reversed[::-1]
-        #return None # TODO - Implement solution
+        return string_to_be_reversed[::-1]
+
+#print(StringManipulator.get_hello_world(None))
