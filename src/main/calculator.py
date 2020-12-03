981861ca8358c2d43622dbcd91ecd442838ea47b
Paul Ouellette <pouellette123es@gmail.com>
2020-12-03 13:18:30 -0500

    updated source - removed tabs

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,16 +1,16 @@
 # Created by Leon Hunter at 9:54 AM 10/23/2020
 class Calculator(object):
     def add(self, a, b):
-        return a+b #None # TODO - Implement solution
+        return a+b
 
     def subtract(self, a, b):
-        return a-b #None # TODO - Implement solution
+        return a-b
 
     def multiply(self, a, b):
-        return a*b #None # TODO - Implement solution
+        return a*b
 
     def divide(self, a, b):
-        return a/b # None # TODO - Implement solution
+        return a/b
 
 #print(Calculator.add(None,3.2, 4))
 #print(Calculator.subtract(None,3.2, 4))
